3f600d4f799a0df94db08500fd4deecbf6417da5
Stefan <s.fuhrberg@ostfalia.de>
2022-11-16 14:49:23 +0100

    AbstractCommand Änderungen

--- src/main/java/de/ostfalia/s3/control/commands/AbstractCommand.java
+++ src/main/java/de/ostfalia/s3/control/commands/AbstractCommand.java
@@ -1,14 +1,20 @@
 package de.ostfalia.s3.control.commands;
 
+import de.ostfalia.s1.lamp.AbstractLampController;
 import lombok.Getter;
 
 public abstract class AbstractCommand implements ICommand {
 
     @Getter
     private String name;
+    @Getter
+    private AbstractLampController controller;
 
-    public AbstractCommand(String name){
+    public AbstractCommand(AbstractLampController controller, String name){
         this.name = name;
+        this.controller = controller;
     }
+
+
 
 }
