80ca4e47b9adcbb2a6f6dd02223d186234ab39c0
martendziersan <ma.dziersan@ostfalia.de>
2022-11-16 23:31:58 +0100

    weitere GUI Tests

--- src/main/java/de/ostfalia/s3/boundary/RemoteControlView.java
+++ src/main/java/de/ostfalia/s3/boundary/RemoteControlView.java
@@ -1,15 +1,34 @@
 package de.ostfalia.s3.boundary;
 
 import de.ostfalia.s3.control.CommandProcessor;
+import lombok.Getter;
+import lombok.Setter;
 
-public class RemoteControlView {
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Named
+@SessionScoped
+public class RemoteControlView implements Serializable {
 
     private CommandProcessor commandProcessor;
+
+    private List<Integer> freePositions = new ArrayList<>(8);
 
     public void switchButton(){
     }
 
     public void colorButton(){
 
+    }
+    public void initList(){
+        for (int i = 1; i  < 9; i++){
+            freePositions.add(i);
+        }
     }
 }
