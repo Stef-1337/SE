44120afc01b0004f0a41887bdb75de97fa6b2ffe
martendziersan <ma.dziersan@ostfalia.de>
2022-11-18 10:40:14 +0100

    GUI Ansatz für Auswahl freie Felder

--- src/main/java/de/ostfalia/s3/boundary/RemoteControlView.java
+++ src/main/java/de/ostfalia/s3/boundary/RemoteControlView.java
@@ -3,8 +3,11 @@ package de.ostfalia.s3.boundary;
 import de.ostfalia.s3.control.CommandProcessor;
 import lombok.Getter;
 import lombok.Setter;
+import org.primefaces.component.selectonemenu.SelectOneMenu;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.model.SelectItem;
+import javax.faces.model.SelectItemGroup;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -18,7 +21,28 @@ public class RemoteControlView implements Serializable {
 
     private CommandProcessor commandProcessor;
 
+
+    private int positionSelected;
     private List<Integer> freePositions = new ArrayList<>(8);
+
+
+    public RemoteControlView(){
+        for (int i = 1; i  < 9; i++) {
+            freePositions.add(i);
+        }
+    }
+
+    public List<Integer> showFreePositions(){
+        return null;
+    }
+    public void init(){
+        List<SelectItem> items = new ArrayList<>();
+        SelectOneMenu menu = new SelectOneMenu();
+        SelectItemGroup group = new SelectItemGroup();
+        for (Integer i: freePositions) {
+            items.add(new SelectItem(i));
+        }
+    }
 
     public void switchButton(){
     }
@@ -26,9 +50,5 @@ public class RemoteControlView implements Serializable {
     public void colorButton(){
 
     }
-    public void initList(){
-        for (int i = 1; i  < 9; i++){
-            freePositions.add(i);
-        }
-    }
+
 }
