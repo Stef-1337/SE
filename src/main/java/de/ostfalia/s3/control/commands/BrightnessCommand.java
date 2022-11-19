4c422dbf119dfae55ed1b32b516ccf7fb7ed98c8
id021960 <l.jacobsen@ostfalia.de>
2022-11-19 17:02:37 +0100

    execute methode

--- src/main/java/de/ostfalia/s3/control/commands/BrightnessCommand.java
+++ src/main/java/de/ostfalia/s3/control/commands/BrightnessCommand.java
@@ -2,17 +2,17 @@ package de.ostfalia.s3.control.commands;
 
 import de.ostfalia.s1.lamp.AbstractLampController;
 
-public class BrightnessCommand extends AbstractCommand{
+public class BrightnessCommand extends AbstractCommand {
 
-private int brightness;
+    private float brightness;
 
-    public BrightnessCommand(AbstractLampController controller, String name, int brightness) {
+    public BrightnessCommand(AbstractLampController controller, String name, float brightness) {
         super(controller, name);
         this.brightness = brightness;
     }
 
     @Override
     public void execute(AbstractLampController controller) {
-
+        controller.brightnessChanged(brightness);
     }
 }
