7e2343857ad0962289adb274981029870f3f0d5c
martendziersan <ma.dziersan@ostfalia.de>
2022-11-19 13:29:43 +0100

    Vorbereitungen für 2 Views und featureGUI2

--- src/main/java/de/ostfalia/s3/boundary/RemoteControlView.java
+++ src/main/java/de/ostfalia/s3/boundary/RemoteControlView.java
@@ -1,7 +1,6 @@
 package de.ostfalia.s3.boundary;
 
 import de.ostfalia.s1.lamp.AbstractLampController;
-import de.ostfalia.s1.lamp.Lamp;
 import de.ostfalia.s3.control.CommandParameterData;
 import de.ostfalia.s3.control.CommandProcessor;
 import de.ostfalia.s3.control.commands.AbstractCommand;
@@ -11,9 +10,10 @@ import lombok.Setter;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
-import java.awt.*;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 @Getter
@@ -24,14 +24,25 @@ public class RemoteControlView implements Serializable {
 
     private static final int SIZE = 8;
 
+    private List<Integer> slots;
+
     private CommandProcessor commandProcessor;
 
     private HashMap<Integer, AbstractCommand> commands = new HashMap<>();
 
     private AbstractLampController controller;
 
-    private int slot;
+    private int slotSelected;
+
     private CommandParameterData data = new CommandParameterData();
+
+    public RemoteControlView(){
+        slots = new ArrayList<>(SIZE);
+
+        for (int i = 1; i < 9; i++){
+            slots.add(i);
+        }
+    }
 
     public void setCommand(int slot, AbstractCommand command) {
         if (slot < SIZE) commands.put(slot, command);
@@ -42,11 +53,11 @@ public class RemoteControlView implements Serializable {
     }
 
     public void onButtonClick() {
-        getCommand(slot).ifPresent(command -> commandProcessor.execute(command));
+        getCommand(slotSelected).ifPresent(command -> commandProcessor.execute(command));
     }
 
     public void onApplySwitchButtonClick() {
-        setCommand(slot, new StateCommand(controller, data.getName(), data.isOn()));
+        setCommand(slotSelected, new StateCommand(controller, data.getName(), data.isOn()));
     }
 
     public void onApplyColorButtonClick(){
--- src/main/java/de/ostfalia/s3/control/CommandParameterData.java
+++ src/main/java/de/ostfalia/s3/control/CommandParameterData.java
@@ -3,11 +3,17 @@ package de.ostfalia.s3.control;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.awt.*;
+import java.util.List;
+
 @Getter
 @Setter
 public class CommandParameterData {
 
     private String name;
     private boolean on;
+    private double intensity;
+    private double intensityStep;
+    private List<Color> colors;
 
 }
