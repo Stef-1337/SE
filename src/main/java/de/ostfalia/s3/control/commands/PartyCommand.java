b5fcfd84fdeda5d36b308e54abec6abf7aa5b8b2
id021960 <l.jacobsen@ostfalia.de>
2022-11-27 15:29:29 +0100

    party bugs behoben

--- src/main/java/de/ostfalia/s3/control/commands/PartyCommand.java
+++ src/main/java/de/ostfalia/s3/control/commands/PartyCommand.java
@@ -31,22 +31,27 @@ public class PartyCommand extends AbstractThreadCommand {
             new LampCommand(controller, "lamp", true,curBrightness, colors.get(randomNum)).execute(controller);
             int change = 0;
             int count = 0;
-            int amount = 10 + random.nextInt() * (40);
-            System.out.println(amount + "amount");
+            int amount = 0;
 
             try {
                 while (!getThread().isInterrupted()) {
+                    if (curBrightness <= 5){
+                        change = 2;
+                    }
                     while (curBrightness > 5 && change == 0) {
+                        System.out.println(000000000000000);
                         new BrightnessCommand(controller, "brightness", curBrightness -= 5).execute(controller);
                         Thread.sleep(1);
                         if (curBrightness == brightness - 15 || curBrightness <= 6) {
                             change = random.nextInt(3);
+                            amount =  random.nextInt((40 - 10) + 1) + 10;
                             if (curBrightness <= 5) {
                                 change = 1;
                             }
                         }
                     }
                     if (change == 1) {
+                        System.out.println(1111111111);
                         curBrightness = brightness / 3;
                         new StateCommand(controller, "state", false).execute(controller);
                         Thread.sleep(100);
@@ -55,23 +60,34 @@ public class PartyCommand extends AbstractThreadCommand {
                         change = random.nextInt(3);
                     }
                     while (curBrightness < 90 && change == 2) {
+                        System.out.println(222222222);
                         new BrightnessCommand(controller, "brightness", curBrightness += 10).execute(controller);
                         if (curBrightness >= 89) {
                             change = random.nextInt(3);
+                            amount =  random.nextInt((40 - 10) + 1) + 10;
                             if (change == 2) {
                                 change = 3;
                             }
                         }
                     }
                     while (change == 3) {
+                        System.out.println(3333333);
                         randomNum = random.nextInt(size);
                         HueColor randomcolor = colors.get(randomNum);
                         count++;
                         new ColorCommand(controller, "color", randomcolor).execute(controller);
                         Thread.sleep(100);
                         if (count == amount) {
+                            System.out.println("fehler 1");
                             change = random.nextInt(3);
+                            System.out.println("fehler 2");
                             count = 0;
+                            amount =  random.nextInt((40 - 10) + 1) + 10;
+                            System.out.println("fehler 3" + change);
+                            if (change == 2 && controller.getAdapter().getIntensity() >= 89){
+                                System.out.println("fehler 4");
+                                change = 0;
+                            }
                         }
                     }
                 }
