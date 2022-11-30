f5df98691155476d04d34df0b56d5aa2a7124236
Stefan <s.fuhrberg@ostfalia.de>
2022-11-30 15:26:14 +0100

    Letzte Anpassungen

--- src/main/java/de/ostfalia/s2/fahrrad/boundary/BicycleDetailView.java
+++ src/main/java/de/ostfalia/s2/fahrrad/boundary/BicycleDetailView.java
@@ -72,10 +72,14 @@ public class BicycleDetailView implements Serializable {
 
     public BicycleDetailView(){
         if(timeRange == null || timeRange.isEmpty()){
-            timeRange = new ArrayList<>();
-            timeRange.add(java.sql.Timestamp.valueOf(LocalDateTime.now().minusHours(1)));
-            timeRange.add(java.sql.Timestamp.valueOf(LocalDateTime.now()));
+            initTimeRange();
         }
+    }
+
+    public void initTimeRange(){
+        timeRange = new ArrayList<>();
+        timeRange.add(java.sql.Timestamp.valueOf(LocalDateTime.now().minusHours(1)));
+        timeRange.add(java.sql.Timestamp.valueOf(LocalDateTime.now()));
     }
 
     public void resetStep() {
@@ -83,6 +87,12 @@ public class BicycleDetailView implements Serializable {
         s = "automatisch festgelegt";
         timeUnit = TimeUnit.HOURS;
         caches = null;
+    }
+
+    public void clear(){
+        resetStep();
+        initTimeRange();
+        if(caches != null) caches.clear();
     }
 
     public String getNum(int i, double d1, double d2, boolean b) {
