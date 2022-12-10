3f75ceb26ac1caf0ce35092506d872c674804406
Jan <j.hertzig@ostfalia.de>
2022-12-10 14:45:09 +0100

    Beispiel zu nutzung Singleton

--- src/main/java/de/ostfalia/s3/entity/Datacollector.java
+++ src/main/java/de/ostfalia/s3/entity/Datacollector.java
@@ -1,54 +1,19 @@
 package de.ostfalia.s3.entity;
 
-import de.ostfalia.s2.fahrrad.control.BicycleService;
-import de.ostfalia.s2.fahrrad.entity.Bicycle;
-import de.ostfalia.s2.fahrrad.kennzahl.strategy.KennzahlDistance;
-import de.ostfalia.s2.fahrrad.kennzahl.strategy.KennzahlSpeed;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
 
-import java.time.LocalDateTime;
-import java.util.*;
+@Named
+@ViewScoped
+public class Datacollector implements Serializable {
 
+        @Inject
+        BicycleData db;
 
-public class Datacollector {
-    int id;
-
-    int time;
-    private static Datacollector datacollector = null;
-    BicycleService bs = new BicycleService();
-
-    private Datacollector(int id, int time){
-        this.id = id;
-        this.time = time;
-        System.out.println("private");
-    }
-
-    public static Datacollector getInstance(int id, int time){
-        if (datacollector == null) {
-            datacollector = new Datacollector(id, time);
-            System.out.println("private");
+        public String getCounter(){
+            return Integer.toString(db.getCounter());
         }
-        System.out.println("static");
-        return datacollector;
-    }
 
-
-    public double speed(){
-        List<Bicycle> list =  bs.getFahrradDaten(id, LocalDateTime.now(), LocalDateTime.now().minusMinutes(time), 1);
-        KennzahlSpeed tmp = new KennzahlSpeed();
-        return tmp.getAverage(list);
-    }
-
-
-    public double distanz(int channel){
-        if (channel != id) {
-            setId(channel);
-        }
-            List<Bicycle> list = bs.getFahrradDaten(id, LocalDateTime.now(), LocalDateTime.now().minusMinutes(time), 1);
-        KennzahlDistance tmp = new KennzahlDistance();
-        return tmp.getTotal(list);
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 }
